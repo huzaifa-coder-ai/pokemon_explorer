cd7d0f9ca44a778530fe45380705efe9593dd39c
Ameer Hamza <ameernormie@gmail.com>
2025-07-30 14:44:11 +0500

    feat: implement pokemon card

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,45 @@ import PokemonCard from "./components/PokemonCard";
 
 const App = () => {
   const [pokemonList, setpokemonList] = useState([]);
- 
+  const [isLoading, setLoading] = useState(false);
 
-    useEffect(() => {
-      fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
-        .then((res) => res.json())
-        .then((data) => setpokemonList(data.results))
-        .catch((error) => console.log("Error occured :", error));
-    }, []);
-
-
-
-
-  return (
-  <>
-  <h1>Pokemon Explorer</h1>
-  <div>
-
-      {pokemonList.map((pokemon) => {
-        return (
-          <PokemonCard key={pokemon.name} name={pokemon.name} url={pokemon.url} />
-        );
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
+      .then((res) => res.json())
+      .then((data) => {
+        setpokemonList(data.results);
+        setLoading(false);
       })
-    }
-  </div>
-</>
-  )
+      .catch((error) => {
+        setLoading(false);
+        console.log("Error occured :", error);
+      });
+  }, []);
 
-}
-
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  return (
+    <>
+      <h1>Pokemon Explorer</h1>
+      <div>
+        {pokemonList.map((pokemon) => {
+          return (
+            <PokemonCard
+              key={pokemon.name}
+              name={pokemon.name}
+              url={pokemon.url}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,35 +2,47 @@
 import { useEffect, useState } from "react";
 import "./Card.css";
 
-const PokemonCard = (props) => {
-  const imageUrl = `https://img.pokemondb.net/sprites/home/normal/${props.name}.png`;
-  const [details, setdetails] = useState(false)
-  
-  const showInfo = () =>{
-    setdetails(!details)
-  }
+const PokemonCard = ({ name: pokemonName, url }) => {
+  const imageUrl = `https://img.pokemondb.net/sprites/home/normal/${pokemonName}.png`;
+  const [showDetails, setShowDetails] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [pokemonDetails, setPokemonDetails] = useState();
+  console.log("[test] url ", url);
 
-    
+  useEffect(() => {
+    setLoading(true);
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log("[test] response ", res);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log("[test] error loading details ", err);
+        setLoading(false);
+      });
+  }, [url]);
 
- 
-  
+  const showInfo = () => {
+    setShowDetails(!showDetails);
+  };
 
   return (
     <div className="card">
-      <h2>{props.name}</h2>
-      <i 
-      onClick={showInfo}
-      className={`fa-solid fa-caret-down ${details ? `up` : `down`} `}></i>
-      <img src={imageUrl} alt={props.name} />
-      {details && (
+      <h2>{pokemonName}</h2>
+      <i
+        onClick={showInfo}
+        className={`fa-solid fa-caret-down ${showDetails ? `up` : `down`} `}
+      ></i>
+      <img src={imageUrl} alt={pokemonName} />
+      {isLoading && <h1>Loading details</h1>}
+      {showDetails && (
         <div className="details-section">
-            <p>Abilities :-</p>
+          <p>Abilities :-</p>
         </div>
       )}
     </div>
-    
-); 
-
-}; 
+  );
+};
 
 export default PokemonCard;
